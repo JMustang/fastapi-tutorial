c6deaf987bfa03de6b501a8663edc2f3a40de94f
Junior <eecfredes@gmail.com>
2026-04-04 20:24:31 -0300

    :rocket:Query parameter

--- app/app.py
+++ app/app.py
@@ -2,19 +2,23 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-text_post = {1: {"title": "1 post", "content": "code post"}}
-text_post = {2: {"title": "2 post", "content": "car post"}}
-text_post = {3: {"title": "3 post", "content": "money post"}}
-text_post = {4: {"title": "4 post", "content": "music post"}}
-text_post = {5: {"title": "5 post", "content": "books post"}}
-text_post = {6: {"title": "6 post", "content": "snickers post"}}
-text_post = {7: {"title": "7 post", "content": "food post"}}
-text_post = {8: {"title": "8 post", "content": "hobby post"}}
-text_post = {9: {"title": "9 post", "content": "work post"}}
+text_post = {
+    1: {"title": "1 post", "content": "code post"},
+    2: {"title": "2 post", "content": "car post"},
+    3: {"title": "3 post", "content": "money post"},
+    4: {"title": "4 post", "content": "music post"},
+    5: {"title": "5 post", "content": "books post"},
+    6: {"title": "6 post", "content": "snickers post"},
+    7: {"title": "7 post", "content": "food post"},
+    8: {"title": "8 post", "content": "hobby post"},
+    9: {"title": "9 post", "content": "work post"},
+}
 
 
 @app.get("/posts")
-def get_all_posts():
+def get_all_posts(limit: int = None):
+    if limit:
+        return list(text_post.values())[:limit]
     return text_post
 
 
