8221e1e17aba3b4d168bbe33279a552f84ce4e8f
Junior <eecfredes@gmail.com>
2026-04-09 21:31:14 -0300

    :rocket: DB conn

--- app/db.py
+++ app/db.py
@@ -1,9 +1,15 @@
 from collections.abc import AsyncGenerator
 import uuid
+import os
+from dotenv import load_dotenv
 
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 
-DATABASE_URL = ""
+load_dotenv()
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+asyncpg://fastapi_user:fastapi_password@localhost:5432/fastapi_db",
+)
