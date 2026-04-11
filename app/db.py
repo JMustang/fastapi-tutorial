1dba0c792c464c2fd0ae7c99f0b7cbcd5910bba2
Junior <eecfredes@gmail.com>
2026-04-10 22:02:29 -0300

    :rocket: Post model

--- app/db.py
+++ app/db.py
@@ -7,9 +7,23 @@ from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
+import datetime
 
-load_dotenv()
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+asyncpg://fastapi_user:fastapi_password@localhost:5432/fastapi_db",
-)
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+# load_dotenv()
+# DATABASE_URL = os.getenv(
+#     "DATABASE_URL",
+#     "postgresql+asyncpg://fastapi_user:fastapi_password@localhost:5432/fastapi_db",
+# )
+
+
+class Post(DeclarativeBase):
+    __tablename__ = "posts"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    caption = Column(Text)
+    url = Column(String, nullable=False)
+    file_type = Column(String, nullable=False)
+    file_name = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
